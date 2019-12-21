f937ec3512de5ee2233430fb5c0aab6c889cfc3a
Veljko <51533885+Veljko998@users.noreply.github.com>
2019-12-21 20:35:51 +0100

    Login clasa koja ce samo ispisivati na konzolu

---
+++ WebProj/src/rest/Login.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package rest;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/** 
+ * @author Veljko
+ * @since 21.12.2019.
+ */
+@Path("/user")
+public class Login {
+	@POST
+	@Path("/login")
+	public Response logIn(
+		@FormParam("username") String username,
+		@FormParam("password") String password,
+		@FormParam("remember") Boolean remember) {
+		return Response.status(200).entity("Username: " + username + ", i sifra i pamcenje").build();
+	}
+}
