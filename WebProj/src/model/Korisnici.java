a4c8853624053f2b5107a44154718aea22d2b24b
Ivana <Ivana@DESKTOP-PV06MG4>
2019-12-23 23:21:38 +0100

    Radi upisivanje ne radi ucitavanje iz fajla

--- WebProj/src/model/Korisnici.java
+++ WebProj/src/model/Korisnici.java
@@ -1,9 +1,14 @@
 package model;
 
+import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.time.LocalDateTime;
+
+import model.enums.Uloga;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -33,6 +38,7 @@ public class Korisnici {
 			for(Korisnik k : korisniciLista){
 				korisniciMapa.put(k.getEmail(), k);
 			}
+			System.out.println(korisniciMapa);
 			
 		}catch(JsonParseException e){
 			
@@ -46,15 +52,27 @@ public class Korisnici {
 		}
 	}
 	
-	public boolean UpisiKorisnike(){
-		try{
+	public List<Korisnik> UpisiKorisnike(){
+		try{/*
+			Korisnik k = new Korisnik("mail", "Ime1", "Prezime1",
+					new Organizacija(), Uloga.KORISNIK,
+					 new ArrayList<Tuple<LocalDateTime, LocalDateTime>>());
+			Korisnik k2 = new Korisnik("mail", "Ime1", "Prezime1",
+					new Organizacija(), Uloga.KORISNIK,
+					 new ArrayList<Tuple<LocalDateTime, LocalDateTime>>());
+			
+			korisniciLista.add(k);
+			korisniciLista.add(k2);
+			*/
 			ObjectMapper mapper = new ObjectMapper();
-			//mapper.writeValue(new File(putanja), korisniciLista);
+			this.putanja = "C:\\Users\\Ivana\\git\\WebProj\\WebProj\\WebContent\\korisnici.json";
+			mapper.writeValue(new File(putanja), korisniciLista);
+			return korisniciLista;
 			
 			
 		}catch(Exception e){
-			
+			return korisniciLista;
 		}
-		return false;
+		
 	}
 }
--- WebProj/src/rest/GetData.java
+++ WebProj/src/rest/GetData.java
@@ -7,13 +7,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import model.Korisnici;
 import model.Korisnik;
-/*
+
+@Path("/data")  
 public class GetData {
 	@GET
 	@Path("/getallusers")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Korisnik> getKorisnici() {
-		
+		Korisnici k = new Korisnici("C:\\Users\\Ivana\\git\\WebProj\\WebProj\\WebContent\\korisnici.json");
+		return k.UpisiKorisnike();
 	}
-}*/
+}
