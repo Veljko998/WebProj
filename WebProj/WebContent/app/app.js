f72da296070d7e786b7481fd329bd05665cc8b1d
Veljko <51533885+Veljko998@users.noreply.github.com>
2019-12-24 13:58:05 +0100

    simple vue works in index.html

--- WebProj/WebContent/app/app.js
+++ WebProj/WebContent/app/app.js
@@ -1,19 +1,8 @@
-var global = {count : 0}; //Ne znam sta ovo znaci i da li treba da se nalazi ovde
+let data = {
+		username: 'your username'
+	};
 
-var app = new Vue({
-    el: "#logout",
-    data: {
-        fromIspis1 : 'Poruka o fromIChild1 ...'
-    },
-	mounted() {
-		this.$root.$on('messageToParent', (text) => {
-			this.fromChild1 = text + ', count : ' + global.count;
-		});
-	},
-    methods: {
-    	sendToChild1: function() {
-    		global.count++;
-    		this.$root.$emit('messageFromParent', 'Hello my child');
-    	}
-    } 
-})
+var app = new Vue({ 
+    el: '#idlogin',
+	data: data
+});
