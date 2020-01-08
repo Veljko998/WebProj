b6aea0be93aef885a0e8a90eb44c351070d80e96
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-01-08 13:41:58 +0100

    Uradjen navigation bar korisnika.

--- WebProj/WebContent/app/korisnik.js
+++ WebProj/WebContent/app/korisnik.js
@@ -8,9 +8,33 @@ Vue.component("korisnik-page", {
 	},
 	template:
 		`
-<div class="container">
-	<button class="position-absolute">Logout</button>
-</div>
+<nav class="navbar navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">Username text</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarText">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home option<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Option1</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Option2</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Option3</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#/">Logout</a>
+				</li>
+			</ul>
+		</div>
+
+	</nav>
+	<p>Ovde ce ici iz home dugmeta. valjda</p>
 		`,
 		
 });
