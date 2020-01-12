7f46cda432dbc532b86f5ddca070908f3b9542d4
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-01-13 00:13:52 +0100

    Prilagodjen navbar i kod superadmina

--- WebProj/WebContent/app/superadministrator.js
+++ WebProj/WebContent/app/superadministrator.js
@@ -12,30 +12,14 @@ Vue.component("superadministrator-page", {
 		`
 <div class="container-fluid">
 	<nav class="navbar navbar-dark bg-dark">
-		<a class="navbar-brand" href="#">Superadministrator</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarText">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Home option<span class="sr-only">(current)</span></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#" v-on:click="showOrganisationsTable = !showOrganisationsTable">Pregled organizacija</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#" v-on:click="showUsersTable = !showUsersTable">Pregled korisnika</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Option3</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#/">Logout</a>
-				</li>
-			</ul>
+		<div class="div-inline">
+			<button class="btn btn-light" type="button" v-on:click="GoToPregledVM();">Pregled VM</button>
+			<button class="btn btn-sm btn-outline-secondary" type="button"  v-on:click="showPregledKor();">Pregled korisnika</button>
+			<button class="btn btn-sm btn-outline-secondary" type="button"  v-on:click="showOrganisationsTable = !showOrganisationsTable">Pregled organizacija</button>
+			
 		</div>
-
+		
+		<button class="btn btn-outline-success my-2 my-sm-0" type="button" v-on:click="removeRole();">Logout</button>
 	</nav>
 	</br>
 	<pregled-korisnika v-if="showUsersTable"></pregled-korisnika>
@@ -43,5 +27,20 @@ Vue.component("superadministrator-page", {
 	
 </div>
 		`,
-		
+	methods: {
+		doNothing: function(){
+			someMessage = localStorage.getItem('role');
+			console.log(someMessage);
+		},
+		removeRole: function(){
+			localStorage.setItem("role", "noRole");
+			router.push({path: "/"});
+		},
+		GoToPregledVM: function(){
+			router.push({path: "/pregledVM"});
+		},
+		showPregledKor: function(){
+			this.showUsersTable = !this.showUsersTable;
+		}
+	},
 });
